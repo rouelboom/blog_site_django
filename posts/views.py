cd6ee05989690de4d411d40289ba4ee338f0444b
Pavel <pavel@ya.ru>
2021-02-28 15:40:08 +0300

    сделал заготовку

--- posts/views.py
+++ posts/views.py
@@ -124,3 +124,20 @@ def page_not_found(request, exception):
 
 def server_error(request):
     return render(request, "misc/500.html", status=500)
+
+@login_required
+def follow_index(request):
+    # информация о текущем пользователе доступна в переменной request.user
+    # ...
+    return render(request, "follow.html", {})
+
+@login_required
+def profile_follow(request, username):
+    # ...
+    pass
+
+
+@login_required
+def profile_unfollow(request, username):
+    # ...
+    pass
